b3e16a100f0e7ed2b2e4ef5921b4d32defe32341
zvonlexa <zvon.lexa@yandex.ru>
2019-01-06 18:56:37 +0300

    first commit

---
+++ resources/js/core/Router.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <transition mode="out-in"
+        enter-active-class="fadeIn"
+        leave-active-class="fadeOut">
+        <router-view class="animated"
+            :key="key"/>
+    </transition>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Router',
+
+    computed: {
+        key() {
+            return JSON.stringify(this.$route.params);
+        },
+    },
+};
+
+</script>
---
+++ resources/js/modules/enso/plugins/date-fns/i18n.js
@@ -0,0 +1,15 @@
+import ro from 'date-fns/esm/locale/ro';
+import en from 'date-fns/esm/locale/en-US/';
+import de from 'date-fns/esm/locale/de';
+import nl from 'date-fns/esm/locale/nl';
+import fr from 'date-fns/esm/locale/fr';
+// import pt from 'date-fns/esm/locale/pt';
+// import ar from 'date-fns/esm/locale/ar';
+// import mn from 'date-fns/esm/locale/mn';
+import ru from 'date-fns/esm/locale/ru';
+
+const i18n = {
+    ro, en, de, nl, fr, br: en, ar: en, mn: en, ru,
+};
+
+export default i18n;
